8967ecb4c68eff4d81dedfcfeddd057d3c3c2ad5
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-07-24 19:13:50 +0300

    chore: Update database schema and models

--- prisma/migrations/20240724123210_init/migration.sql
+++ prisma/migrations/20240724123210_init/migration.sql
@@ -6,8 +6,9 @@ CREATE TYPE "SubscriptionStatus" AS ENUM ('ACTIVE', 'EXPIRED', 'CANCELED');
 
 -- CreateTable
 CREATE TABLE "Account" (
-    "id" BIGSERIAL NOT NULL,
-    "userId" BIGINT NOT NULL,
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "publicId" TEXT NOT NULL,
     "type" TEXT NOT NULL,
     "provider" TEXT NOT NULL,
     "providerAccountId" TEXT NOT NULL,
@@ -26,8 +27,9 @@ CREATE TABLE "Account" (
 
 -- CreateTable
 CREATE TABLE "User" (
-    "id" BIGSERIAL NOT NULL,
+    "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
+    "publicId" TEXT NOT NULL,
     "firstName" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
     "password" TEXT NOT NULL,
@@ -43,9 +45,9 @@ CREATE TABLE "User" (
 
 -- CreateTable
 CREATE TABLE "Session" (
-    "id" BIGSERIAL NOT NULL,
+    "id" SERIAL NOT NULL,
     "sessionToken" TEXT NOT NULL,
-    "userId" BIGINT NOT NULL,
+    "userId" INTEGER NOT NULL,
     "expires" TIMESTAMP(3) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
@@ -55,7 +57,7 @@ CREATE TABLE "Session" (
 
 -- CreateTable
 CREATE TABLE "VerificationToken" (
-    "id" BIGSERIAL NOT NULL,
+    "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "token" TEXT NOT NULL,
     "expires" TIMESTAMP(3) NOT NULL,
@@ -65,7 +67,7 @@ CREATE TABLE "VerificationToken" (
 
 -- CreateTable
 CREATE TABLE "TwoFactorToken" (
-    "id" BIGSERIAL NOT NULL,
+    "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "token" TEXT NOT NULL,
     "expires" TIMESTAMP(3) NOT NULL,
@@ -75,15 +77,15 @@ CREATE TABLE "TwoFactorToken" (
 
 -- CreateTable
 CREATE TABLE "TwoFactorConfirmation" (
-    "id" BIGSERIAL NOT NULL,
-    "userId" BIGINT NOT NULL,
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
 
     CONSTRAINT "TwoFactorConfirmation_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "PasswordResetToken" (
-    "id" BIGSERIAL NOT NULL,
+    "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "token" TEXT NOT NULL,
     "expires" TIMESTAMP(3) NOT NULL,
@@ -93,10 +95,11 @@ CREATE TABLE "PasswordResetToken" (
 
 -- CreateTable
 CREATE TABLE "Table" (
-    "id" BIGSERIAL NOT NULL,
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
+    "publicId" TEXT NOT NULL,
     "isPublic" BOOLEAN NOT NULL DEFAULT false,
-    "userId" BIGINT NOT NULL,
+    "userId" INTEGER NOT NULL,
     "libraryId" INTEGER,
 
     CONSTRAINT "Table_pkey" PRIMARY KEY ("id")
@@ -105,8 +108,9 @@ CREATE TABLE "Table" (
 -- CreateTable
 CREATE TABLE "Subject" (
     "id" SERIAL NOT NULL,
+    "publicId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "tableId" BIGINT NOT NULL,
+    "tableId" INTEGER NOT NULL,
     "absences" INTEGER NOT NULL DEFAULT 4,
 
     CONSTRAINT "Subject_pkey" PRIMARY KEY ("id")
@@ -115,6 +119,7 @@ CREATE TABLE "Subject" (
 -- CreateTable
 CREATE TABLE "Library" (
     "id" SERIAL NOT NULL,
+    "publicId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "tableId" INTEGER NOT NULL,
 
@@ -123,30 +128,31 @@ CREATE TABLE "Library" (
 
 -- CreateTable
 CREATE TABLE "PublicTableView" (
-    "id" BIGSERIAL NOT NULL,
-    "userId" BIGINT NOT NULL,
-    "tableId" BIGINT NOT NULL,
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "tableId" INTEGER NOT NULL,
 
     CONSTRAINT "PublicTableView_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "PrivateTableView" (
-    "id" BIGSERIAL NOT NULL,
-    "userId" BIGINT NOT NULL,
-    "tableId" BIGINT NOT NULL,
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "tableId" INTEGER NOT NULL,
 
     CONSTRAINT "PrivateTableView_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Subscription" (
-    "id" BIGSERIAL NOT NULL,
+    "id" SERIAL NOT NULL,
     "price" DOUBLE PRECISION NOT NULL,
+    "publicId" TEXT NOT NULL,
     "subscriptionPeriod" "SubscriptionPeriod" NOT NULL,
     "subscribedAt" TIMESTAMP(3) NOT NULL,
     "expiresAt" TIMESTAMP(3) NOT NULL,
-    "userId" BIGINT NOT NULL,
+    "userId" INTEGER NOT NULL,
     "subscriptionStatus" "SubscriptionStatus" NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
@@ -155,10 +161,16 @@ CREATE TABLE "Subscription" (
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "Account_publicId_key" ON "Account"("publicId");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_publicId_key" ON "User"("publicId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Session_sessionToken_key" ON "Session"("sessionToken");
@@ -188,19 +200,37 @@ CREATE UNIQUE INDEX "PasswordResetToken_token_key" ON "PasswordResetToken"("toke
 CREATE UNIQUE INDEX "PasswordResetToken_email_token_key" ON "PasswordResetToken"("email", "token");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "Table_name_key" ON "Table"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Table_publicId_key" ON "Table"("publicId");
+
+-- CreateIndex
 CREATE INDEX "Table_isPublic_idx" ON "Table"("isPublic");
 
 -- CreateIndex
 CREATE INDEX "Table_userId_idx" ON "Table"("userId");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "Subject_publicId_key" ON "Subject"("publicId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Subject_name_key" ON "Subject"("name");
+
+-- CreateIndex
 CREATE INDEX "Subject_tableId_idx" ON "Subject"("tableId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Library_publicId_key" ON "Library"("publicId");
 
 -- CreateIndex
 CREATE INDEX "PublicTableView_userId_tableId_idx" ON "PublicTableView"("userId", "tableId");
 
 -- CreateIndex
 CREATE INDEX "PrivateTableView_userId_tableId_idx" ON "PrivateTableView"("userId", "tableId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Subscription_publicId_key" ON "Subscription"("publicId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Subscription_userId_key" ON "Subscription"("userId");
