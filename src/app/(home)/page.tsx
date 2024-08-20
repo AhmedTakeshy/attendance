bec96159d48f096047ef7fd20c9adbb1268a0468
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-08-20 18:02:14 +0300

    refactor: Update Home page component to handle table creation and display

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -11,23 +11,38 @@ export default async function Home() {
   const session = await auth();
   const response = await getLastAttendanceTable(returnPublicId(session?.user.id as string));
   return (
-    response?.status === "Success" ? (
-      <SimpleAttendanceTable table={response.data.table} />
+    session?.user ? (
+      response?.status === "Success" ? (
+        <SimpleAttendanceTable table={response.data.table} />
+      ) : (
+        <CreateTableSection path="create" userId={`${session.user.id}`} words={words} />
+      )
     ) : (
-      <>
-        <div className="flex justify-center items-center px-4 mx-auto">
-          <div className="text-4xl mx-auto font-normal text-slate-600 dark:text-slate-500">
-            Create
-            <FlipWords words={words} /> <br />
-            tables to track you attendance
-          </div>
-        </div>
-        <Button asChild>
-          <Link href={`/login`} className="flex items-center bg-blue-500 hover:bg-blue-700 transition-colors duration-300">
-            Create attendance table
-          </Link>
-        </Button>
-      </>
+      <CreateTableSection path="login" words={words} />
     )
   );
+}
+
+type CreateTableSectionProps = {
+  path: "login" | "create",
+  userId?: string,
+  words: string[]
+}
+function CreateTableSection({ path, words, userId }: CreateTableSectionProps) {
+  return (
+    <>
+      <div className="flex justify-center items-center px-4 mx-auto">
+        <div className="text-4xl mx-auto font-normal text-slate-600 dark:text-slate-500">
+          Create
+          <FlipWords words={words} /> <br />
+          tables to track you attendance
+        </div>
+      </div>
+      <Button asChild>
+        <Link href={`/${path === "login" ? "login" : userId}/create-table`} className="flex items-center bg-blue-500 hover:bg-blue-700 transition-colors duration-300">
+          Create attendance table
+        </Link>
+      </Button>
+    </>
+  )
 }
