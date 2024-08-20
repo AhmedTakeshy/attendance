a3cc5134d70c685f1721c40e104e7fbd737ece56
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-08-20 17:48:47 +0300

    chore: Update loginWithProvider component to use Twitter login instead of Facebook

--- src/app/(home)/(auth)/_components/loginWithProvider.tsx
+++ src/app/(home)/(auth)/_components/loginWithProvider.tsx
@@ -6,22 +6,22 @@ import { RiTwitterXLine, RiInstagramLine } from "react-icons/ri"
 import { FcGoogle } from "react-icons/fc"
 
 export default function LoginWithProvider() {
-    const [isPending, setIsPending] = useState<boolean>(false)
+    const [isPending, setIsPending] = useState<{ [key: string]: boolean }>({})
 
     async function loginWithProvider(provider: OAuthProvider) {
-        setIsPending(true)
+        setIsPending(prev => ({ ...prev, [provider.type]: true }))
         try {
             await login(provider)
         } catch (error) {
             return { error: true, message: "Something went wrong!", status: 401 }
         }
-        setIsPending(false)
+        setIsPending(prev => ({ ...prev, [provider.type]: false }))
     }
     return (
         <div className="flex flex-col space-y-4">
             <SubmitButton
-                disabled={isPending}
-                pending={isPending}
+                disabled={isPending["google"] || false}
+                pending={isPending["google"] || false}
                 onClick={() => loginWithProvider({ type: "google" })}
                 className="hover:cursor-pointer relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
             >
@@ -32,20 +32,20 @@ export default function LoginWithProvider() {
                 <BottomGradient />
             </SubmitButton>
             <SubmitButton
-                disabled={isPending}
-                pending={isPending}
+                disabled={isPending["twitter"] || false}
+                pending={isPending["twitter"] || false}
                 onClick={() => loginWithProvider({ type: "twitter" })}
                 className="hover:cursor-pointer relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
             >
                 <RiTwitterXLine className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
                 <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-                    Facebook
+                    X
                 </span>
                 <BottomGradient />
             </SubmitButton>
             <SubmitButton
-                disabled={isPending}
-                pending={isPending}
+                disabled={isPending["instagram"] || false}
+                pending={isPending["instagram"] || false}
                 onClick={() => loginWithProvider({ type: "instagram" })}
                 className="hover:cursor-pointer relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
             >
